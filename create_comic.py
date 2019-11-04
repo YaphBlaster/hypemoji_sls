687129809bbb9e3c23955901f4a4668c46ada164
YaphBlaster <yaphet.abraha@gmail.com>
2019-11-04 15:33:18 -0600

    Code cleanup

--- create_comic.py
+++ create_comic.py
@@ -11,9 +11,14 @@ from imgurpython import ImgurClient
 import boto3
 import botocore
 import os
+import json
 
 
 def create_comic(event, context):
+    is_remote = False
+    if __name__ != "__main__":
+        is_remote = True;
+
     try:
         list_im = []
         list_text = []
@@ -23,60 +28,61 @@ def create_comic(event, context):
             list_text.append(value['text'])
 
         images = [Image.open(i) for i in list_im]
-
-        s3 = boto3.resource('s3').Bucket('imgurupload')
-
-        try:
-            s3.download_file(
-                'tmp/Hack-BoldOblique.ttf', '/tmp/font.ttf')
-        except botocore.exceptions.ClientError as e:
-            print("Error: ", e)
-
-        # use a truetype font
-        font = ImageFont.truetype('/tmp/font.ttf', 22)
-
+        
+        if(is_remote):
+            s3 = boto3.resource('s3').Bucket('imgurupload')
+            try:
+                s3.download_file(
+                    'tmp/Hack-BoldOblique.ttf', '/tmp/font.ttf')
+            except botocore.exceptions.ClientError as e:
+                print("Error: ", e)
+            font = ImageFont.truetype('/tmp/font.ttf', 22)
+            
+            LAMBDA_IMGUR_CLIENT_ID = os.environ['IMGUR_CLIENT_ID']
+            LAMBDA_IMGUR_CLIENT_SECRET = os.environ['IMGUR_CLIENT_SECRET']
+            client = ImgurClient(LAMBDA_IMGUR_CLIENT_ID, LAMBDA_IMGUR_CLIENT_SECRET)
+        else:
+            font = ImageFont.truetype("arial.ttf", 22)
+ 
         widths, heights = zip(*(i.size for i in images))
-
-        total_height = sum(heights)
+        gap_in_pixels = 5
+        total_height = sum(heights) + (gap_in_pixels * len(images))
         max_width = max(widths)
 
         new_vertical_image = Image.new('RGB', (max_width, total_height))
-
-        x_offset = 0
-        y_offset = 0
-        index = 0
         border_color = "black"
+
+        y_offset = 0;
         for index, image, in enumerate(images):
             draw = ImageDraw.Draw(image, "RGBA")
+
             w, h = draw.textsize(list_text[index].upper(), font)
+            width, height = image.size;
             # thicker border
-            newWidth = (398-w)/2
-            newHeight = (350-h)
+            newWidth = (width-w)/2
+            newHeight = (height-h) - 10
             newText = list_text[index].upper()
             draw.text((newWidth+1, newHeight-1),
-                      newText, font=font, fill=border_color)
+                    newText, font=font, fill=border_color)
             draw.text((newWidth-1, newHeight+1),
-                      newText, font=font, fill=border_color)
+                    newText, font=font, fill=border_color)
             draw.text((newWidth+1, newHeight+1),
-                      newText, font=font, fill=border_color)
+                    newText, font=font, fill=border_color)
             draw.text((newWidth-1, newHeight-1),
-                      newText, font=font, fill=border_color)
-            draw.text(((398-w)/2, (350-h)),
-                      newText, font=font, fill=(255, 255, 255, 255))
+                    newText, font=font, fill=border_color)
+            draw.text((newWidth, newHeight),
+                    newText, font=font, fill=(255, 255, 255, 255))
 
             new_vertical_image.paste(image, (0, y_offset))
-            x_offset += image.size[0]
-            y_offset += image.size[1]
-
+            y_offset += image.size[1] + gap_in_pixels
+        
         new_vertical_image.save('/tmp/verticalImage.png',
                                 format='PNG', subsampling=0, quality=100)
 
-        LAMBDA_IMGUR_CLIENT_ID = os.environ['IMGUR_CLIENT_ID']
-        LAMBDA_IMGUR_CLIENT_SECRET = os.environ['IMGUR_CLIENT_SECRET']
-        client = ImgurClient(LAMBDA_IMGUR_CLIENT_ID,
-                             LAMBDA_IMGUR_CLIENT_SECRET)
-
-        uploaded_image = client.upload_from_path("/tmp/verticalImage.png")
+        if(is_remote):
+            uploaded_image = client.upload_from_path("/tmp/verticalImage.png")
+        else:
+            uploaded_image = {'link': "tmp/verticalImage.png"}
 
         response = {
             'statusCode': 200,
@@ -90,3 +96,9 @@ def create_comic(event, context):
         }
 
     return response
+
+
+if __name__ == "__main__":
+    with open('createComicTest.json') as f:
+        data = json.load(f)
+    print(create_comic(data, ''))
